4da41f3221faae36cdd4f408e9950ed1c9c5b712
Nacho Monzón <ignaciomonzona@gmail.com>
2024-11-30 10:43:05 +0100

    Iteration 1 complete

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,17 @@
 import { Navbar } from "./components/ui"
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './pages/home';
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} /> {/* EL NOMBRE DEL COMPONENTE EN MAYÚSCULA (NECESITO MÁS CAFÉ) */}
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+
+        return (
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quod
+            expedita cupiditate aliquam facere atque sequi accusamus
+            reprehenderit vero perferendis ratione doloremque eos reiciendis
+            deserunt veniam tenetur ducimus, soluta incidunt?
+          </h1>
+        );
+
+}
+
+export default Home; 
